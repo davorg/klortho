85b9a0828eb9c52cbf519c2b5d4d22aa79894c87
Dave Cross <dave@dave.org.uk>
2011-09-17 14:49:17 +0100

    Initial commit.

---
+++ klortho.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Dancer;
+use XML::RSS;
+use Klortho;
+
+get '/' => sub {
+    return Klortho::advice(params->{n});
+};
+
+get '/rss' => sub {
+    content_type 'text/xml';
+    
+    my $advice = Klortho::advice(params->{n});
+    
+    my $rss = XML::RSS->new(version => '1.0');
+    $rss->channel(
+        title => 'Advice from Klortho',
+        link  => 'http://dave.org.uk/klortho/rss/',
+        description => 'Advice from Klortho',
+    );
+
+    my ($n) = $advice =~ /(\d+)/;
+
+    my $link = "http://dave.org.uk/klortho/?n=$n";
+    
+    $rss->add_item(
+        title => $advice,
+        description => $advice,
+        link => $link,
+    );
+    
+    return $rss->as_string;
+};
+
+dance;
